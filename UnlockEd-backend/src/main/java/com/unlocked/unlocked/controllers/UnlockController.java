e779a6bfa89d17df2d5068e466f1581d557fcd79
Likithach1945 <68991899+Likithach1945@users.noreply.github.com>
2025-08-20 12:06:43 +0530

    Update UnlockController.java

--- UnlockEd-backend/src/main/java/com/unlocked/unlocked/controllers/UnlockController.java
+++ UnlockEd-backend/src/main/java/com/unlocked/unlocked/controllers/UnlockController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173","http://localhost:3000", "https://unlockedu.netlify.app"})
 @RestController
 @RequestMapping("/api/unlock")
 public class UnlockController {
